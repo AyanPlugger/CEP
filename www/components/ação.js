e259f4058d422eb25fe817ab1ffd4e74411551df
AyanPlugger <ayanpruggertattoo@gmail.com>
2021-08-16 23:25:20 +0900

    Project's Initial Commit

---
+++ www/components/ação.js
@@ -0,0 +1,21 @@
+
+
+$(document).on("click","#buscar",function(){
+  $.ajax({
+    type:"get",
+    url: "https://viacep.com.br/ws/"+$("#cep").val()+"/json/",
+    success: function(data){
+        var conteudo;
+        conteudo = "<p><strong>Logradouro: </strong>"+data.logradouro+"</p>";
+        conteudo += "<p><strong>Bairro: </strong>"+data.bairro+"</p>";
+        conteudo += "<p><strong>Cidade: </strong>"+data.localidade+"</p>";
+        conteudo += "<p><strong>Estado: </strong>"+data.uf+"</p>";
+
+        $("#dados").html(conteudo);
+    },
+    error: function(){
+      alert("deu erro");
+    }
+
+  });
+})
